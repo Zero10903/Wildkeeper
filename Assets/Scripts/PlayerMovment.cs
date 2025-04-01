44232a6df42b5af971d8697e19853fedd2df24f7
benja-bay <benjaminbayarri17@gmail.com>
2025-04-01 19:37:34 -0300

    syntax adjustment
    Syntax and variable name adjustments in the Player Movement code

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -1,16 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerMovment : MonoBehaviour
 {
     [SerializeField] private float speed = 3f;
     
-    private Rigidbody2D playerRb2d;
-    private Vector2 moveInput;
+    private Rigidbody2D _playerRb2d;
+    private Vector2 _moveInput;
     void Start()
     {
-        playerRb2d = GetComponent<Rigidbody2D>();
+        _playerRb2d = GetComponent<Rigidbody2D>();
     }
 
     
@@ -19,11 +18,11 @@ public class PlayerMovment : MonoBehaviour
         //inputs
         float moveX = Input.GetAxisRaw("Horizontal"); //horizontal movement of the player
         float moveY = Input.GetAxisRaw("Vertical"); //vertical movement of the player
-        moveInput = new Vector2(moveX, moveY).normalized;
+        _moveInput = new Vector2(moveX, moveY).normalized;
     }
 
     void FixedUpdate()
     {
-        playerRb2d.MovePosition(playerRb2d.position + moveInput * speed * Time.fixedDeltaTime);
+        _playerRb2d.MovePosition(_playerRb2d.position + _moveInput * speed * Time.fixedDeltaTime);
     }
 }
