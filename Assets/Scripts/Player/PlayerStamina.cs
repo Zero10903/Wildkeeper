d831a7781b42f38c7dadfb7edc59091e178e947a
emmazabala <141878559+emmazabala@users.noreply.github.com>
2025-04-09 18:50:02 -0300

    refactor: renamed functions and added tabs.
    Renombramos RegenStamina por Regen. Eliminamos el booleano _isRunning porque no es utilizado. Tambien se añadieron tabs

--- Assets/Scripts/Player/PlayerStamina.cs
+++ Assets/Scripts/Player/PlayerStamina.cs
@@ -10,24 +10,23 @@ public class PlayerStamina : MonoBehaviour
     [SerializeField] private float _walkSpeed = 4f;
     [SerializeField] private float _runSpeed = 6f;
     [SerializeField] private float _staminaRegenPerSecond = 3f;
-    private bool _isRunning = false;
     public PlayerMovement playerMovement;
 
     private void Start(){ playerMovement.SetSpeed(_walkSpeed); }
     void Update()
     { 
-        if(Input.GetKey(KeyCode.LeftShift) && _actualStamina > 0f) { 
-         if(playerMovement.IsMoving){ Run();}
-         else{ RegenStamina();}
-        }else { RegenStamina(); }  
+        if(Input.GetKey(KeyCode.LeftShift) && _actualStamina > _runStaminaCost) { 
+            if(playerMovement.IsMoving){ Run();}
+            else{ Regen();}
+        } else { Regen(); }  
     }
     void Run()
     { 
-      playerMovement.SetSpeed(_runSpeed);
-      _actualStamina -= _runStaminaCost * Time.deltaTime;
-      _actualStamina = Mathf.Clamp(_actualStamina, 0f, _maxStamina);
+        playerMovement.SetSpeed(_runSpeed);
+        _actualStamina -= _runStaminaCost * Time.deltaTime;
+        _actualStamina = Mathf.Clamp(_actualStamina, 0f, _maxStamina);
     }
-    void RegenStamina() 
+    void Regen() 
     {
         playerMovement.SetSpeed(_walkSpeed);
         _actualStamina += _staminaRegenPerSecond * Time.deltaTime;
