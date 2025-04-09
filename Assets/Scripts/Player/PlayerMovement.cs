c02303f7a14bc936e3ca079cbe274271cbc84b37
Benjamin Bayarri <benjaminbayarri17@gmail.com>
2025-04-09 03:02:52 -0300

    feat: player movement animation added

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,18 +6,24 @@ public class PlayerMovement : MonoBehaviour  {
     
     private Rigidbody2D _rb2D;
     private Vector2 _moveInput;
+    private Animator _animator;
     
     [Header("Movement")]
     [SerializeField] private float speed = 4f;
 
     void Awake() {
         _rb2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
     
     void Update() {
         float moveX = Input.GetAxisRaw("Horizontal"); //? horizontal movement of the player
         float moveY = Input.GetAxisRaw("Vertical"); //? vertical movement of the player
         _moveInput = new Vector2(moveX, moveY).normalized;
+        
+        _animator.SetFloat("Horizontal", moveX);
+        _animator.SetFloat("Vertical", moveY);
+        _animator.SetFloat("Speed", _moveInput.sqrMagnitude);
     }
 
     void FixedUpdate() {
