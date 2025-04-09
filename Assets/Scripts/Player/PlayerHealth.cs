cdfd28cd22b4e90b835bbde6fa22b156ad588808
emmazabala <141878559+emmazabala@users.noreply.github.com>
2025-04-09 18:58:11 -0300

    feat: Function Death added and modified Heal and Damage from PlayerHealth
    Se añadio una funcion ded muerte para cuando la vida actual es menor que 0. Se cambio el Heal y Damage porque el player podia alcanzar vida negativa y sumar por encima de MaxHealth.

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -9,11 +9,21 @@ public class PlayerHealth : MonoBehaviour
 
     private void Start() {_actualHealth = _maxHealth;}
 
-    public void Damage(float damage) { _actualHealth -= damage; }
+    public void Damage(float damage) 
+    { 
+        _actualHealth -= damage;
+        if (_actualHealth < 0)
+        {
+            Death();
+        }
+    }
 
     public void Heal(float heal)
     {
-        if (_actualHealth >= _maxHealth) { return; }
-        if (_actualHealth <= _maxHealth) { _actualHealth += heal; }
+        if (_actualHealth > _maxHealth) { _actualHealth = _maxHealth; }
+        if (_actualHealth < _maxHealth) { _actualHealth += heal; }
+        if (_actualHealth == _maxHealth) { Debug.Log("No puedes curarte mas, Vida maxima alcanzada"); }
     }
+
+    public void Death() {Debug.Log("Dead");}
 }
